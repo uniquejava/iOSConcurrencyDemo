f940f69422c982af6b7dd2423d0acb6a7f3352ae
cyper <345343747@qq.com>
2016-10-27 17:54:12 +0800

    simple OperationQueue(concurrent)

--- iOSConcurrencyDemo/ViewController.swift
+++ iOSConcurrencyDemo/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sliderValueLabel: UILabel!
     
+    var queue = OperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -37,12 +39,38 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didClickOnStart(_ sender: AnyObject) {
+        queue = OperationQueue()
+        queue.addOperation {
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+            OperationQueue.main.addOperation({
+                self.imageView1.image = img1
+            })
+        }
+        queue.addOperation {
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            OperationQueue.main.addOperation({
+                self.imageView2.image = img2
+            })
+        }
+        queue.addOperation {
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            OperationQueue.main.addOperation({
+                self.imageView3.image = img3
+            })
+        }
+        queue.addOperation {
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            OperationQueue.main.addOperation({
+                self.imageView4.image = img4
+            })
+        }
+
         //let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) // old
         //let queue = DispatchQueue.global(qos: .default) // global concurrent queue
         
         //let queue = dispatch_queue_create("com.cyper.xxx", DISPATCH_QUEUE_SERIAL)
         //let queue = DispatchQueue(label: "com.cyper.xxx", qos: .userInitiated)
-        
+        /*
         let queue = DispatchQueue(label: "com.cyper.xxx", qos: .userInitiated, attributes: .concurrent)
         
         queue.async {
@@ -72,6 +100,8 @@ class ViewController: UIViewController {
                 self.imageView4.image = img4
             }
         }
+        */
+        
     }
     
     @IBAction func didClickOnCancel(_ sender: AnyObject) {
